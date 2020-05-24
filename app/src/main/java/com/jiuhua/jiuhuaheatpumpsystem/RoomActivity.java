f4497e2eb4a4e81b168b00313f15372195a9fd13
zhyongcn <286168826@qq.com>
2020-05-24 15:47:42 +0800

    久和老李的软件

--- app/src/main/java/com/jiuhua/jiuhuaheatpumpsystem/RoomActivity.java
+++ app/src/main/java/com/jiuhua/jiuhuaheatpumpsystem/RoomActivity.java
@@ -10,11 +10,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import java.util.Calendar;
 
 import com.jiuhua.mqttsample.IGetMessageCallBack;
 import com.jiuhua.mqttsample.MQTTService;
 import com.jiuhua.mqttsample.MyServiceConnection;
+
+import java.util.Calendar;
 
 
 public class RoomActivity extends AppCompatActivity implements IGetMessageCallBack, View.OnClickListener {
@@ -30,10 +31,10 @@ public class RoomActivity extends AppCompatActivity implements IGetMessageCallBa
     private Button btManualOn;//手动开
     private Button btAutomation;//自动运行
     private Button btSetPeroid;//宴会
-    String roomName="";
-    String roomID="";
-    String temperature="";
-    String humidity="";
+    String roomName = "";
+    String roomID = "";
+    String temperature = "";
+    String humidity = "";
 
     private Context context;
     private String peroid_S;
@@ -62,10 +63,10 @@ public class RoomActivity extends AppCompatActivity implements IGetMessageCallBa
         roomName = intent.getStringExtra("roomname");  //Mainactivity传过来的点击的哪一个按钮（房间）
         roomID = intent.getStringExtra("roomid");  //传一个标识
         tvRoomName.setText(roomName);
-        if (calendar.get(Calendar.MONTH)<9 && calendar.get(Calendar.MONTH)>3) {
-            peroid_S = sharedPreferences.getString("Room"+roomID+"summerperoid_S", "");//利用标识，获取存储的周期数据
+        if (calendar.get(Calendar.MONTH) < 9 && calendar.get(Calendar.MONTH) > 3) {
+            peroid_S = sharedPreferences.getString("Room" + roomID + "summerperoid_S", "");//利用标识，获取存储的周期数据
         } else {
-            peroid_S = sharedPreferences.getString("Room"+roomID+"winterperoid_S", "");
+            peroid_S = sharedPreferences.getString("Room" + roomID + "winterperoid_S", "");
         }
 
         context = this;
@@ -81,28 +82,28 @@ public class RoomActivity extends AppCompatActivity implements IGetMessageCallBa
         Intent intentServer = new Intent(this, MQTTService.class);//传递信息
         bindService(intentServer, serviceConnection, Context.BIND_AUTO_CREATE);//绑定服务
 
-        MQTTService.publish("86518/JYCFGC/6-2-3401/Room"+roomID,
-                "Room"+roomID+"feedback", 1, true);
+        MQTTService.publish("86518/JYCFGC/6-2-3401/Room" + roomID,
+                "Room" + roomID + "feedback", 1, true);
     }
 
     @Override
     public void setMessage(String message) {
         //依据message字符串最后一位决定房间号，倒数第二位决定温湿度C为温度，H为湿度。
-        if(message.contains("C"+roomID)) temperature = message.replace("C" + roomID, "C");
-        if(message.contains("RH"+roomID)) humidity = message.replace("RH" + roomID, "RH");
+        if (message.contains("C" + roomID)) temperature = message.replace("C" + roomID, "C");
+        if (message.contains("RH" + roomID)) humidity = message.replace("RH" + roomID, "RH");
         //回传的模块状态
-        if(message.contains("turnoffRoom"+roomID+"FP")) tvModuleState.setText("    停止");
-        if(message.contains("manualRoom"+roomID+"FP")) tvModuleState.setText("    手动");
-        if(message.contains("automationRoom"+roomID+"FP")) tvModuleState.setText("    自动");
-        if(message.contains("dehumidityRoom"+roomID+"FP")) tvModuleState.setText("    除湿");
+        if (message.contains("turnoffRoom" + roomID + "FP")) tvModuleState.setText("    停止");
+        if (message.contains("manualRoom" + roomID + "FP")) tvModuleState.setText("    手动");
+        if (message.contains("automationRoom" + roomID + "FP")) tvModuleState.setText("    自动");
+        if (message.contains("dehumidityRoom" + roomID + "FP")) tvModuleState.setText("    除湿");
         //回传的风机状态
-        if(message.contains("lowRoom"+roomID+"FP")) tvFanState.setText("    低风速");
-        if(message.contains("middleRoom"+roomID+"FP")) tvFanState.setText("    中风速");
-        if(message.contains("highRoom"+roomID+"FP")) tvFanState.setText("    高风速");
-        if(message.contains("stopRoom"+roomID+"FP")) tvFanState.setText("    风速停");
+        if (message.contains("lowRoom" + roomID + "FP")) tvFanState.setText("    低风速");
+        if (message.contains("middleRoom" + roomID + "FP")) tvFanState.setText("    中风速");
+        if (message.contains("highRoom" + roomID + "FP")) tvFanState.setText("    高风速");
+        if (message.contains("stopRoom" + roomID + "FP")) tvFanState.setText("    风速停");
         //地暖模块回传的信息
-        if(message.contains("valveonRoom"+roomID+"floor")) tvFloorState.setText("    地暖运行");
-        if(message.contains("valveoffRoom"+roomID+"floor")) tvFloorState.setText("    地暖停止");
+        if (message.contains("valveonRoom" + roomID + "floor")) tvFloorState.setText("    地暖运行");
+        if (message.contains("valveoffRoom" + roomID + "floor")) tvFloorState.setText("    地暖停止");
 
         tvTemperature.setText(temperature);
         tvHumidity.setText(humidity);
@@ -117,22 +118,22 @@ public class RoomActivity extends AppCompatActivity implements IGetMessageCallBa
 
         switch (v.getId()) {
             case R.id.turn_off:
-                MQTTService.publish("86518/JYCFGC/6-2-3401/Room"+roomID,
-                        "Room"+roomID+"turn-offFP", 1, true);
-                MQTTService.publish("86518/JYCFGC/6-2-3401/Room"+roomID,
-                        "Room"+roomID+"turn-offfloor", 1, true);
+                MQTTService.publish("86518/JYCFGC/6-2-3401/Room" + roomID,
+                        "Room" + roomID + "turn-offFP", 1, true);
+                MQTTService.publish("86518/JYCFGC/6-2-3401/Room" + roomID,
+                        "Room" + roomID + "turn-offfloor", 1, true);
                 //设置各个按钮的颜色
                 btManualOff.setBackgroundColor(Color.parseColor("#00FF00"));//设置手动停止按钮背景为绿色
-                btManualOn.setBackgroundColor(Color.argb(20,0,0,0));//设置手动的按钮背景为灰色
-                btAutomation.setBackgroundColor(Color.argb(20,0,0,0));//设置自动按钮的背景为灰色
-                btSetPeroid.setBackgroundColor(Color.argb(20,0,0,0));//设置宴会按钮的背景为灰色
+                btManualOn.setBackgroundColor(Color.argb(20, 0, 0, 0));//设置手动的按钮背景为灰色
+                btAutomation.setBackgroundColor(Color.argb(20, 0, 0, 0));//设置自动按钮的背景为灰色
+                btSetPeroid.setBackgroundColor(Color.argb(20, 0, 0, 0));//设置宴会按钮的背景为灰色
                 break;
             case R.id.manual_on:
                 //设置各个按钮的颜色
-                btManualOff.setBackgroundColor(Color.argb(20,0,0,0));//设置手动停止按钮背景为绿色
+                btManualOff.setBackgroundColor(Color.argb(20, 0, 0, 0));//设置手动停止按钮背景为绿色
                 btManualOn.setBackgroundColor(Color.parseColor("#00FF00"));//设置手动的按钮背景为灰色
-                btAutomation.setBackgroundColor(Color.argb(20,0,0,0));//设置自动按钮的背景为灰色
-                btSetPeroid.setBackgroundColor(Color.argb(20,0,0,0));//设置宴会按钮的背景为灰色
+                btAutomation.setBackgroundColor(Color.argb(20, 0, 0, 0));//设置自动按钮的背景为灰色
+                btSetPeroid.setBackgroundColor(Color.argb(20, 0, 0, 0));//设置宴会按钮的背景为灰色
 
                         /*实现数字选择器，比较经典，保留参考！！
                         //用数字选择器实现温度temperature选择
@@ -164,10 +165,10 @@ public class RoomActivity extends AppCompatActivity implements IGetMessageCallBa
                         */
 
                 //先开阀，再考虑温度，是不是应该开。
-                if (calendar.get(Calendar.MONTH)<9 && calendar.get(Calendar.MONTH)>3) {   //通过月份来判断冬季还是夏季
-                    MQTTService.publish("86518/JYCFGC/6-2-3401/Room"+roomID,"Room"+roomID+"manual-onFP", 1, true);
+                if (calendar.get(Calendar.MONTH) < 9 && calendar.get(Calendar.MONTH) > 3) {   //通过月份来判断冬季还是夏季
+                    MQTTService.publish("86518/JYCFGC/6-2-3401/Room" + roomID, "Room" + roomID + "manual-onFP", 1, true);
                 } else {
-                    MQTTService.publish("86518/JYCFGC/6-2-3401/Room"+roomID,"Room"+roomID+"manual-onfloor", 1, true);
+                    MQTTService.publish("86518/JYCFGC/6-2-3401/Room" + roomID, "Room" + roomID + "manual-onfloor", 1, true);
                 }
                 //跳转到一个单独的设置页面（ManualActivity）
                 Intent intent = new Intent(RoomActivity.this, ManualActivity.class);
@@ -177,16 +178,16 @@ public class RoomActivity extends AppCompatActivity implements IGetMessageCallBa
 
                 break;
             case R.id.automation:
-                if (calendar.get(Calendar.MONTH)<9 && calendar.get(Calendar.MONTH)>3) {    //通过月份来判断冬季还是夏季
-                    MQTTService.publish("86518/JYCFGC/6-2-3401/Room"+roomID,"Room"+roomID+"automationFP", 1, true);
+                if (calendar.get(Calendar.MONTH) < 9 && calendar.get(Calendar.MONTH) > 3) {    //通过月份来判断冬季还是夏季
+                    MQTTService.publish("86518/JYCFGC/6-2-3401/Room" + roomID, "Room" + roomID + "automationFP", 1, true);
                 } else {
-                    MQTTService.publish("86518/JYCFGC/6-2-3401/Room"+roomID,"Room"+roomID+"automationfloor", 1, true);
+                    MQTTService.publish("86518/JYCFGC/6-2-3401/Room" + roomID, "Room" + roomID + "automationfloor", 1, true);
                 }
                 //设置各个按钮的颜色
-                btManualOff.setBackgroundColor(Color.argb(20,0,0,0));//设置手动停止按钮背景为绿色
-                btManualOn.setBackgroundColor(Color.argb(20,0,0,0));//设置手动的按钮背景为灰色
+                btManualOff.setBackgroundColor(Color.argb(20, 0, 0, 0));//设置手动停止按钮背景为绿色
+                btManualOn.setBackgroundColor(Color.argb(20, 0, 0, 0));//设置手动的按钮背景为灰色
                 btAutomation.setBackgroundColor(Color.parseColor("#00FF00"));//设置自动按钮的背景为灰色
-                btSetPeroid.setBackgroundColor(Color.argb(20,0,0,0));//设置宴会按钮的背景为灰色
+                btSetPeroid.setBackgroundColor(Color.argb(20, 0, 0, 0));//设置宴会按钮的背景为灰色
                 break;
             case R.id.setperoid:
                 //跳转到一个单独的设置页面（SetPeroidActivity）
